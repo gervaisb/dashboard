60203a56500e4623ad710c7ff62cd84c1bc62134
Gervais Blaise <gervais.b@gmail.com>
2013-11-18 08:32:02 +0100

    Firts commit of Atlassian's Bamboo CI server

--- impls/filesystem/src/main/java/be/gervaisb/oss/dashboard/repos/maven/fileystem/RepositoryFailureException.java
+++
@@ -1,9 +0,0 @@
-package be.gervaisb.oss.dashboard.repos.maven.fileystem;
-
-public class RepositoryFailureException extends RuntimeException {
-
-    public RepositoryFailureException(final LocalRepository repository, final Throwable cause) {
-	// TODO Auto-generated constructor stub
-    }
-
-}
