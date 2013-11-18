3ebfce0e5c9888e4d1be0423f6d2570f05f7e75e
Gervais Blaise <gervais.b@gmail.com>
2013-11-18 08:31:26 +0100

    Add 'Module' support

--- impls/nexus/src/main/java/be/gervaisb/oss/dashboard/repos/maven/nexus/NexusArtifactBuilder.java
+++ impls/nexus/src/main/java/be/gervaisb/oss/dashboard/repos/maven/nexus/NexusArtifactBuilder.java
@@ -6,6 +6,8 @@ import org.apache.maven.model.Model;
 import org.apache.maven.model.Parent;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import be.gervaisb.oss.dashboard.Application;
 import be.gervaisb.oss.dashboard.Artifact;
@@ -22,6 +24,8 @@ import be.gervaisb.oss.dashboard.repos.maven.ArtifactNotFoundException;
 import be.gervaisb.oss.dashboard.repos.maven.MvnRepository.ArtifactBuilder;
 
 class NexusArtifactBuilder implements ArtifactBuilder {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NexusArtifactBuilder.class);
 
     private final MavenXpp3Reader reader;
     private final Client nexus;
@@ -58,28 +62,63 @@ class NexusArtifactBuilder implements ArtifactBuilder {
 	    return expected.cast(reference);
 	}
 
-	A artifact = null;
-	if ( Application.class.equals(expected) ) {
-	    artifact = (A) new Application(descriptor, model.getBuild().getFinalName());
-	} else if ( Project.class.equals(expected) ) {
-	    Parent parent = model.getParent();
-	    Reference parentReference = parent!=null
-		    ?new Reference.To(parent.getGroupId(), parent.getArtifactId(), new Version(parent.getVersion()))
-	    :Reference.UNKNOW;
-		    StringBuilder description = new StringBuilder();
-		    if ( model.getName()!=null && !model.getName().isEmpty() ) {
-			description.append(model.getName());
-		    }
-		    if ( model.getDescription()!=null && !model.getDescription().isEmpty() ) {
-			description.append(description.length()>0?"<br />":"")
-			.append(model.getDescription());
-		    }
-		    artifact = (A) new Project(reference.getGroupId(), reference.getArtifactId(), reference.getVersion(), parentReference);
-		    ((Project) artifact).setDescription(description.toString());
-	};
+	final String artifactId = reference.getArtifactId();
+	final String groupId = reference.getGroupId();
+	final Version version = reference.getVersion();
 
+	A artifact = null;
+	if (Application.class.equals(expected)) {
+	    Project parent = loadParentProject(model);
+	    String context = model.getBuild().getFinalName();
+
+	    artifact = (A) new Application(groupId, artifactId, version, context, parent);
+
+	} else if (Project.class.equals(expected)) {
+	    Reference parent = parentReference(model);
+
+	    String name = null;
+	    StringBuilder description = new StringBuilder();
+	    if (model.getName() != null && !model.getName().isEmpty()) {
+		name = model.getName();
+		description.append(model.getName());
+	    }
+	    if (model.getDescription() != null && !model.getDescription().isEmpty()) {
+		description.append(description.length() > 0 ? "<br />" : "")
+		.append(model.getDescription());
+	    }
+
+	    artifact = (A) new Project(groupId, artifactId, version, parent);
+	    ((Project) artifact).setName(name);
+	    ((Project) artifact).setDescription(description.toString());
+	} else if (Module.class.equals(expected)) {
+	    Project parent = loadParentProject(model);
+	    artifact = (A) new Module(groupId, artifactId, version, Packaging.valueOfOrNew(model.getPackaging()), parent);
+	}
 	populate(artifact);
+	LOG.debug("{} loaded as {} from {}.", artifact, expected.getSimpleName(), nexus);
 	return artifact;
+    }
+
+    private Project loadParentProject(final Model model) {
+	final Reference reference = parentReference(model);
+	Project parent;
+	try {
+	    parent = new NexusArtifactBuilder(nexus, reference).as(Project.class);
+	} catch (Throwable e) {
+	    parent = new Project.Missing(reference, null);
+	}
+	return parent;
+    }
+
+    private Reference parentReference(final Model model) {
+	Parent descriptor = model.getParent();
+	if ( descriptor==null ) {
+	    return Reference.UNKNOW;
+	} else {
+	    Version version = new Version(descriptor.getVersion());
+	    return new Reference.To(
+		    descriptor.getGroupId(), descriptor.getArtifactId(), version);
+	}
     }
 
     private void populate(final Artifact artifact) {
